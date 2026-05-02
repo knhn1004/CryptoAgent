2cdd1d9d8eaefedc7c14dbd7d3642dcda523d6fe
adarshm11 <amlearning2023@gmail.com>
2026-05-01 23:26:42 -0700

    merge changes

--- go-key-service/cmd/keyserver/main.go
+++ go-key-service/cmd/keyserver/main.go
@@ -1,72 +1,12 @@
 package main
 
 import (
-<<<<<<< HEAD
-	"context"
-	"errors"
-	"log/slog"
-	"net/http"
-	"os"
-	"os/signal"
-	"syscall"
-	"time"
-=======
 	"fmt"
 	"log"
 	"net/http"
->>>>>>> f1c2c79 (Ed25519 keypair generation & registration API)
 
 	"github.com/gorilla/mux"
 	"github.com/knhn1004/CryptoAgent/go-key-service/internal/action"
-<<<<<<< HEAD
-	"github.com/knhn1004/CryptoAgent/go-key-service/internal/config"
-	"github.com/knhn1004/CryptoAgent/go-key-service/internal/httpapi"
-	"github.com/knhn1004/CryptoAgent/go-key-service/internal/keystore"
-)
-
-func main() {
-	cfg, err := config.Load()
-	if err != nil {
-		slog.Error("config", "err", err)
-		os.Exit(1)
-	}
-
-	logger := slog.New(slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: cfg.LogLevel}))
-	slog.SetDefault(logger)
-	logger.Info("starting", "addr", cfg.Addr, "schema_version", action.SchemaVersion)
-
-	store := keystore.NewMemory()
-	srv := &http.Server{
-		Addr:              cfg.Addr,
-		Handler:           httpapi.NewServer(store, logger).Router(),
-		ReadHeaderTimeout: 5 * time.Second,
-	}
-
-	errCh := make(chan error, 1)
-	go func() {
-		if err := srv.ListenAndServe(); err != nil && !errors.Is(err, http.ErrServerClosed) {
-			errCh <- err
-		}
-	}()
-
-	stop := make(chan os.Signal, 1)
-	signal.Notify(stop, os.Interrupt, syscall.SIGTERM)
-
-	select {
-	case sig := <-stop:
-		logger.Info("shutting down", "signal", sig.String())
-	case err := <-errCh:
-		logger.Error("server", "err", err)
-		os.Exit(1)
-	}
-
-	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
-	defer cancel()
-	if err := srv.Shutdown(ctx); err != nil {
-		logger.Error("shutdown", "err", err)
-		os.Exit(1)
-	}
-=======
 	"github.com/knhn1004/CryptoAgent/go-key-service/internal/keys"
 )
 
@@ -75,5 +15,4 @@ func main() {
 	r := mux.NewRouter()
 	keys.RegisterRoutes(r, keys.NewMemoryStore())
 	log.Fatal(http.ListenAndServe(":8080", r))
->>>>>>> f1c2c79 (Ed25519 keypair generation & registration API)
 }
--- go-key-service/go.mod
+++ go-key-service/go.mod
@@ -2,8 +2,7 @@ module github.com/knhn1004/CryptoAgent/go-key-service
 
 go 1.25.0
 
-<<<<<<< HEAD
-require github.com/go-chi/chi/v5 v5.2.5
-=======
-require github.com/gorilla/mux v1.8.1
->>>>>>> f1c2c79 (Ed25519 keypair generation & registration API)
+require (
+	github.com/go-chi/chi/v5 v5.2.5
+	github.com/gorilla/mux v1.8.1
+)
